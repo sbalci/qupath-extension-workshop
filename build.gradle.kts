1b789462e4837cdca42f40298132bbbdc8c7f346
sbalci <drserdarbalci@gmail.com>
2026-05-31 14:18:25 +0300

    chore: bump version to 0.2.0-alpha4

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ plugins {
 }
 
 group = "io.github.sbalci"
-version = "0.2.0-alpha3"
+version = "0.2.0-alpha4"
 description = "QuPath atölyesi: hücre tespiti, IHC kantifikasyonu, tümör/stroma, cTCF — bir menü ile."
 
 java {
