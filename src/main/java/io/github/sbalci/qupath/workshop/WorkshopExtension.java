dbfb8335f4b6ed527182bd0ccf720dbd7c156c8e
sbalci <drserdarbalci@gmail.com>
2026-06-05 16:29:34 +0300

    chore: bump version to 0.2.0

--- src/main/java/io/github/sbalci/qupath/workshop/WorkshopExtension.java
+++ src/main/java/io/github/sbalci/qupath/workshop/WorkshopExtension.java
@@ -322,7 +322,7 @@ public class WorkshopExtension implements QuPathExtension {
 
     @Override
     public Version getVersion() {
-        return Version.parse("0.2.0-alpha5");
+        return Version.parse("0.2.0");
     }
 
     @Override
